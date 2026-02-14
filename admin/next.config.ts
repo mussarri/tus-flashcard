7081f8c47718315794223fa6af27ae18fd6e3864
Mustafa Sarıçiçek <msaricicek_99@hotmail.com>
2026-02-14 06:25:52 +0300

    admin css

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: "/tus-admin",
+  assetPrefix: "/tus-admin", // Statik dosyaların başına /tus-admin ekler
   typescript: {
     // !! TEHLİKELİ !!
     // Projenizde tip hataları olsa bile build alınmasını sağlar.
